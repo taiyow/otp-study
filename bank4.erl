dde165290bb6ebf49348230902379c957c9ef388
Taiyo Watanabe <taiyo.w@gmail.com>
2016-03-13 17:13:15 +0900

    add last version without OTP

--- bank4.erl
+++ bank4.erl
@@ -1,9 +1,8 @@
 -module(bank4).
--export([start/0, restarter/0, loop/1, deposit/2, draw/2, get/1]).
+-export([start/0, restarter/0, loop/1, deposit/2, draw/2, get/1, failure/0]).
 
 % 預金者を区別できるように。
 % 銀行破たん機能追加
-
 -record(state,
 	{failure :: boolean(),
 	 accounts = dict:new()}).
@@ -16,7 +15,7 @@ start() ->
 restarter() ->
     % link先の死亡の巻き添えをふせぐ
     process_flag(trap_exit, true),
-    Pid = spawn_link(?MODULE, loop, [ #state{ failure=false} ]),
+    Pid = spawn_link(?MODULE, loop, [#state{failure=false}]),
     register(bank, Pid),
     receive
 	{'EXIT', Pid, normal} -> % normal exit
@@ -54,55 +53,50 @@ get(Person)  ->
 	    io:format("timeout!!~n")
     end.
 
+failure()  ->
+    bank ! failure.   % 非同期API
 
 %% private loop
 
 loop(#state{failure=Failure,accounts=Accounts}=State) ->
     receive
-	{From, deposit, Person, Money} when is_integer(Money) ->
-            if
-		Failure ->
-		    From ! {error, bank_is_failured},
-	            NewAccounts = Accounts;
-		true ->
-		    case update_balance(Person, Money, Accounts) of
-			{error, no_such_account, _} ->
-			    NewAccounts = dict:store(Person, Money, Accounts),
-			    From ! {ok, Money};
-			{error, Reason, NewAccounts} ->
-			    From ! {error, Reason};
-			{ok, NewBalance, NewAccounts} ->
-			    From ! {ok, NewBalance}
-		    end
-	    end,
-	    loop(State#state{accounts=NewAccounts});
-	{From, draw, Person, Money} when is_integer(Money) ->
-            if
-		Failure ->
-		    From ! {error, bank_is_failured},
-	            NewAccounts = Accounts;
-		true ->
-		    case update_balance(Person, -Money, Accounts) of
-			{error, Reason, NewAccounts} ->
-			    From ! {error, Reason};
-			{ok, NewBalance, NewAccounts} ->
-			    From ! {ok, NewBalance}
-		    end
-	    end,
-	    loop(State#state{accounts=NewAccounts});
-	{From, get, Person} ->
-	    if
-		Failure ->
-		    From ! {ok, 0};
-		true ->
-		    case dict:find(Person, Accounts) of
-			error ->
-			    From ! {error, no_such_account};
-			{ok, Balance} ->
-			    From ! {ok, Balance}
-		end
-	    end,
+	{From, deposit, _, _} when Failure ->
+	    From ! {error, bank_is_failured},
 	    loop(State);
+	{From, deposit, Person, Money} when is_integer(Money) ->
+	    case update_balance(Person, Money, Accounts) of
+		{error, no_such_account, _} ->
+		    NewAccounts = dict:store(Person, Money, Accounts),
+		    From ! {ok, Money};
+		{error, Reason, NewAccounts} ->
+		    From ! {error, Reason};
+		{ok, NewBalance, NewAccounts} ->
+		    From ! {ok, NewBalance}
+	    end,
+	    loop(State#state{accounts=NewAccounts});
+	{From, draw, _, _} when Failure ->
+	    From ! {error, bank_is_failured},
+	    loop(State);
+	{From, draw, Person, Money} when is_integer(Money) ->
+	    case update_balance(Person, -Money, Accounts) of
+		{error, Reason, NewAccounts} ->
+		    From ! {error, Reason};
+		{ok, NewBalance, NewAccounts} ->
+		    From ! {ok, NewBalance}
+	    end,
+	    loop(State#state{accounts=NewAccounts});
+	{From, get, _} when Failure ->
+	    From ! {ok, 0},
+	    loop(State);
+	{From, get, Person} ->
+	    case dict:find(Person, Accounts) of
+		error ->
+		    From ! {error, no_such_account};
+		{ok, Balance} ->
+		    From ! {ok, Balance}
+	    end;
+	failure ->
+	    loop(State#state{failure=true});
 	_ ->
 	    io:format("invalid message~n"),
 	    loop(State)
